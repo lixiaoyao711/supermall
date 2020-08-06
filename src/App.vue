54b1a14e6873d22c4f28a87f89fb00f84748d329
李永富 <1176286624@qq.com>
2020-08-06 10:40:14 +0800

    项目构建

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
+      <h2>s撒飞洒地方</h2>
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
